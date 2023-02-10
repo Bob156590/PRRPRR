ec0b1a3259bfff7b039922ee669c816cf4107d47
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2023-02-10 10:38:19 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Uppgift_6._2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv en number för att potensa");
+            Console.WriteLine(power(double.Parse(Console.ReadLine()), double.Parse(Console.ReadLine())));
+        }
+        static double power(double tal1, double tal2)
+        {
+            return Math.Pow(tal1,tal2);
+        }
+    }
+}
