26de31230aa9a84d25e609a154a47eee16ebd3ac
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2023-02-10 10:08:59 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace övning
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"15+20+13={Addera(15,20,13)}");
+        }
+        static int Addera(int tal1, int tal2, int tal3)
+        {
+            int a = tal1 + tal2 + tal3;
+            return a;
+        }
+    }
+}
