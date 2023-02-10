946de7d9eb782a77a33d8d7716806cc04d257fa7
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2023-02-10 10:15:29 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Uppgift_6._2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"{Störst(1,2)}");
+        }
+        static int Störst(int tal1, int tal2)
+        {
+            if (tal1 == tal2)
+            {
+                return tal1;
+            }
+            else if (tal2 > tal1)
+            {
+                return tal2;
+            }
+            else
+            {
+                return tal1;
+            }
+        }
+    }
+}
