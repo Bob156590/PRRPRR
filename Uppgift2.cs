caa76683adf133beee2648bbd047749d8098484a
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2023-02-17 10:30:43 +0100

    Add files via upload

---
+++ Uppgift2.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace upggift_2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1: Omvandla från Celsius till Fahrenheit\n2: Omvandla från Fahrenheit till Celsius\nVilken omvandling vill du göra? Skriv 1 eller 2:");
+            switch (Console.ReadLine())
+            {
+                case "1":;
+                    Console.Clear();
+                    Console.WriteLine($"meta in Celsius för att få Fagrebgeit");
+                    Console.WriteLine($"{FtillC(int.Parse(Console.ReadLine()))}F");
+                    break;
+                case "2":
+                    Console.Clear();
+                    Console.WriteLine($"meta in Fahrenheit för att få Celsius");
+                    Console.WriteLine($"{FtillC(int.Parse(Console.ReadLine()))} C");
+                    break;
+            }
+        }
+        static double CtillF(double tal)
+        {
+            double a = 32 + tal * 1.8;
+            return a;
+        }
+        static double FtillC(double tal)
+        {
+            double a = (tal-32)/1.8;
+            return a;
+        }
+    }
+}
---
+++ uppgift1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace upggift_1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in två heltal:");
+            Console.WriteLine($"{AREA(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()))}");
+        }
+        static int AREA(int tal1, int tal2)
+        {
+            int a = tal1 * tal2;
+            return a;
+        }
+    }
+}
