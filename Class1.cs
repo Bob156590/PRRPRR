9040ee55de9632b0a3ccbda803db47f92f8714ff
Bob156590 <111962487+Bob156590@users.noreply.github.com>
2023-03-10 11:15:20 +0100

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+       public List<string[]> Bloglista = new List<string[]>();
+
+        public static void Mymethode1()
+        {
+            Console.Clear();
+            Console.WriteLine("Skriv:\n1. För att skriva en blogg\n2. För att visa bloggen\n3. För söka efter bloggen\n4. För att avsluta programet");
+        }
+
+        public void Mymethode2()
+        {
+            int chose = int.Parse(Console.ReadLine());
+            switch (chose)
+            {
+                case 1:
+                    
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default
+                    break;
+            }
+        }
+        public string write()
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using ConsoleApp1;
+
+namespace övning
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Class1 p1 = new Class1();
+            Class1.Mymethode1();
+            p1.Mymethode2();
+        }
+    }
+}
